459e520134b4a481d5957aef594adaf16205709d
teacup143 <82294308+teacup143@users.noreply.github.com>
2021-04-18 20:17:29 -0400

    Added the Weather Icon from the API

--- src/app.js
+++ src/app.js
@@ -21,6 +21,8 @@ function displayTemperature(response){
     let humidityElement = document.querySelector ("#humidity");
     let windElement = document.querySelector ("#wind");
     let dateElement = document.querySelector ("#date");
+    let iconElement = document.querySelector ("#icon");
+
 
 
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
@@ -29,14 +31,15 @@ function displayTemperature(response){
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
     dateElement.innerHTML = formatDate(response.data.dt * 1000);
+    iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`) 
+    iconElement.setAttribute("alt", response.data.weather[0].description); 
 
 
 
 }
 
 let apiKey = "6b10ec582bc419aecce21631ccc1d4f0";
+let city = "New York";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
-
-console.log(apiUrl);
 axios.get(apiUrl).then(displayTemperature);

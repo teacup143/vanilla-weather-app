965e73b9f462c8e9335872d96645dc393fbbab92
teacup143 <82294308+teacup143@users.noreply.github.com>
2021-04-25 13:29:57 -0400

    Concatenate/testing forecast from HTML to JS

--- src/app.js
+++ src/app.js
@@ -13,6 +13,41 @@ function formatDate(timestamp){
     return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+let forecastElement=document.querySelector("#forecast");
+
+let forecastHTML = `<div class="row">`;
+let days = ["Thu", "Fri", "Sat", "Sun"];
+days.forEach(function(day){
+forecastHTML = forecastHTML +  
+  `
+              <div class="col-2">
+                <div class="weather-forecast-date">
+                ${day}
+                </div>
+                <img
+                  src="http://openweathermap.org/img/wn/02d@2x.png"
+                  alt=""
+                  width="42"
+                />
+                <div class="weather-forecast-temperature">
+                  <span class="weather-forecast-temperature-max">
+                    23° </span>
+                  <span class="weather-forecast-temperature-min">
+                    45° </span>
+                  </span>
+                </div>
+               </div>
+              `;
+
+});
+
+
+forecastHTML = forecastHTML + `</div>`;
+forecastElement.innerHTML = forecastHTML;
+}
+
+
 
 function displayTemperature(response){
     let temperatureElement = document.querySelector("#temperature");
@@ -70,6 +105,8 @@ function displayCelsiusTemperature(event) {
 
 let celsiusTemperature = null;
 
+
+
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
@@ -81,3 +118,4 @@ let celsiusLink =document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("New York");
+displayForecast();
